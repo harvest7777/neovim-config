4b7dcbb787e97ebc0ae24daa0605332a8cd4136e
harvset7777 <333harvest.moon333@gmail.com>
2025-02-21 17:14:58 -0800

    keymap update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,3 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>Q", ":qa<CR>", { desc = "Quit All" })
+
+vim.api.nvim_set_keymap("n", "x", '"_x"', { noremap = true, silent = true })
+
+-- Move selected lines up/down in visual mode
+vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+
+-- Move current line up/down in normal mode
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
